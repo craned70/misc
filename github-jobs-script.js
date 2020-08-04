7adeeb690bfb3fdfd9eff125fb45e6d26addcc35
craned70 <craned70@gmail.com>
2020-08-03 21:01:33 -0700

    add ip-api to search jobs by location using IPs

--- github-jobs-script.js
+++ github-jobs-script.js
@@ -1,18 +1,21 @@
 const fetch = require('node-fetch');
-const searchGitJobs = function(description, location) {
-    const url = `https://jobs.github.com/positions.json?description=${description}&location=${location}`.replace(/ /g,"+");
-    return fetch(url)
+const searchGitJobs = function(description) {
+    return fetch("http://ip-api.com/json/")
         .then((res) => res.json())
-        .then((jobs) => {
-            return jobs.map((eachJob) => {
-                return {
-                    title: eachJob.title,
-                    location: eachJob.location,
-                    company: eachJob.company
-                };
-            })
-        })   
+        .then((ip) => ip.city)
+        .then((city) => {
+            const url = `https://jobs.github.com/positions.json?description=${description}&location=${city}`.replace(/ /g,"+");
+            return fetch(url)
+            .then((res) => res.json())
+            .then((jobs) => {
+                return jobs.map((eachJob) => {
+                    return {
+                        title: eachJob.title,
+                        location: eachJob.location,
+                        company: eachJob.company
+                    };
+                })
+        }) 
+        })  
 };
-
-const formattedJobs = searchGitJobs("javascript", "california");
-formattedJobs.then(console.log);
+searchGitJobs("javascript").then(console.log);
