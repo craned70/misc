5456c252c4ab87f77cc4bd6b7cb94a23fd92a056
Dylan Crane <dylan.c@walkme.com>
2020-08-07 12:16:53 -0700

    cleaned up formatting

--- jobs-script-IP.js
+++ jobs-script-IP.js
@@ -1,21 +1,22 @@
 const fetch = require('node-fetch');
-const searchGitJobs = function(description) {
+
+const searchGitJobsIP = function(description) {
     return fetch("http://ip-api.com/json/")
         .then((res) => res.json())
         .then((ip) => ip.city)
         .then((city) => {
             const url = `https://jobs.github.com/positions.json?description=${description}&location=${city}`.replace(/ /g,"+");
             return fetch(url)
-            .then((res) => res.json())
-            .then((jobs) => {
-                return jobs.map((eachJob) => {
-                    return {
-                        title: eachJob.title,
-                        location: eachJob.location,
-                        company: eachJob.company
-                    };
-                })
-        }) 
-        })  
+                .then((res) => res.json())
+                .then((jobs) => {
+                    return jobs.map((eachJob) => {
+                        return {
+                            title: eachJob.title,
+                            location: eachJob.location,
+                            company: eachJob.company
+                        };
+                    });
+                });
+        });
 };
 searchGitJobs("javascript").then(console.log);
